47c42749a4a75701a5642a1f80631f9d6783e033
Nate Matthews <nmatthews@athenahealth.com>
2018-03-18 12:10:30 -0400

    Testing case of uniform values for categorical_score function

--- test_megadescribe.py
+++ test_megadescribe.py
@@ -5,10 +5,11 @@ import string
 import pandas as pd
 from datetime import datetime as dt, timedelta as td
 import pytest
+import sys
 
 def test_typeerror_at_initiation():
     with pytest.raises(TypeError):
-        md.ColumnClassifier(0)
+        md.column_classifier(0)
 
 def test_column_classifications():
     df = pd.DataFrame()
@@ -27,10 +28,29 @@ def test_column_classifications():
     for col in categorical_columns:
         df[col] = pd.Series([random.choice(string.ascii_lowercase) for _ in range(10)])
 
-    instance = md.ColumnClassifier(df)
+    instance = md.column_classifier(df)
     assert set(instance.dates()).issuperset(set(date_columns))
     assert set(instance.numerics()).issuperset(set(numeric_columns))
     assert set(instance.categoricals()).issuperset(set(categorical_columns))
     assert set(instance.dates()).issubset(set(date_columns))
     assert set(instance.numerics()).issubset(set(numeric_columns))
     assert set(instance.categoricals()).issubset(set(categorical_columns))
+
+############################
+# Unusual row finder tests #
+############################
+
+def test_categorical_score_same_value():
+    def score_for_uniform_values(n):
+        randfloat = random.uniform(sys.float_info.min,sys.float_info.max)
+        df = pd.DataFrame({'A':([randfloat] * n)})
+        instance = md.surface_unusual_rows(df)
+        out = instance.categorical_score('A')
+        assert out.name == 'A'
+        assert isinstance(out.index,pd.RangeIndex)
+        assert np.array_equal(out.values,np.array(([0] * n)))
+
+    # We'll try lenth 1, and five randomly sampled lengths between 2 and 100
+    score_for_uniform_values(1)
+    for n in random.sample(range(2,101), 5):
+        score_for_uniform_values(n)
